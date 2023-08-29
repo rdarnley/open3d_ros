4fa1f4d54b62e008509300bc7b55710a4f3e293b
Ryan Darnley <rdarnley@andrew.cmu.edu>
2023-08-28 22:27:53 -0400

    update

--- lib_cloud_conversion_between_Open3D_and_ROS.py
+++ lib_cloud_conversion_between_Open3D_and_ROS.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
-This script contains 2 functions for converting cloud format between Open3D and ROS:   
-* convertCloudFromOpen3dToRos  
-* convertCloudFromRosToOpen3d
-where the ROS format refers to "sensor_msgs/PointCloud2.msg" type.
-
-This script also contains a test case, which does such a thing:  
-(1) Read a open3d_cloud from .pcd file by Open3D.
-(2) Convert it to ros_cloud.
-(3) Publish ros_cloud to topic.
-(4) Subscribe the ros_cloud from the same topic.
-(5) Convert ros_cloud back to open3d_cloud.
-(6) Display it.  
-You can test this script's function by rosrun this script.
-
-'''
-
 import open3d
 import numpy as np
 from ctypes import * # convert float to uint32
@@ -33,8 +16,11 @@ FIELDS_XYZ = [
     PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
     PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
 ]
-FIELDS_XYZRGB = FIELDS_XYZ + \
-    [PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)]
+FIELDS_XYZRGB = [ 
+    PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+    PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+    PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+    PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)]
 
 # Bit operations
 BIT_MOVE_16 = 2**16
@@ -48,12 +34,12 @@ convert_rgbFloat_to_tuple = lambda rgb_float: convert_rgbUint32_to_tuple(
 
 # Convert the datatype of point cloud from Open3D to ROS PointCloud2 (XYZRGB only)
 def convertCloudFromOpen3dToRos(open3d_cloud, frame_id="odom"):
-    # Set "header"
+    print("Converting Cloud From Open3D To ROS")
+
     header = Header()
     header.stamp = rospy.Time.now()
     header.frame_id = frame_id
 
-    # Set "fields" and "cloud_data"
     points=np.asarray(open3d_cloud.points)
     if not open3d_cloud.colors: # XYZ only
         fields=FIELDS_XYZ
@@ -65,8 +51,7 @@ def convertCloudFromOpen3dToRos(open3d_cloud, frame_id="odom"):
         colors = np.floor(np.asarray(open3d_cloud.colors)*255) # nx3 matrix
         colors = colors[:,0] * BIT_MOVE_16 +colors[:,1] * BIT_MOVE_8 + colors[:,2]  
         cloud_data=np.c_[points, colors]
-    
-    # create ros_cloud
+
     return pc2.create_cloud(header, fields, cloud_data)
 
 def convertCloudFromRosToOpen3d(ros_cloud):
@@ -76,7 +61,7 @@ def convertCloudFromRosToOpen3d(ros_cloud):
     cloud_data = list(pc2.read_points(ros_cloud, skip_nans=True, field_names = field_names))
 
     # Check empty
-    open3d_cloud = open3d.PointCloud()
+    open3d_cloud = open3d.geometry.PointCloud()
     if len(cloud_data)==0:
         print("Converting an empty cloud")
         return None
@@ -96,11 +81,11 @@ def convertCloudFromRosToOpen3d(ros_cloud):
             rgb = [convert_rgbUint32_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
 
         # combine
-        open3d_cloud.points = open3d.Vector3dVector(np.array(xyz))
-        open3d_cloud.colors = open3d.Vector3dVector(np.array(rgb)/255.0)
+        open3d_cloud.points = open3d.utility.Vector3dVector(np.array(xyz))
+        open3d_cloud.colors = open3d.utility.Vector3dVector(np.array(rgb)/255.0)
     else:
         xyz = [(x,y,z) for x,y,z in cloud_data ] # get xyz
-        open3d_cloud.points = open3d.Vector3dVector(np.array(xyz))
+        open3d_cloud.points = open3d.utility.Vector3dVector(np.array(xyz))
 
     # return
     return open3d_cloud
@@ -109,22 +94,19 @@ def convertCloudFromRosToOpen3d(ros_cloud):
 if __name__ == "__main__":
     rospy.init_node('test_pc_conversion_between_Open3D_and_ROS', anonymous=True)
     
-    # -- Read point cloud from file
     import os
     PYTHON_FILE_PATH=os.path.join(os.path.dirname(__file__))+"/"
-    if 0: # test XYZ point cloud format
-        filename=PYTHON_FILE_PATH+"test_cloud_XYZ_noRGB.pcd"
-    else: # test XYZRGB point cloud format
-        filename=PYTHON_FILE_PATH+"test_cloud_XYZRGB.pcd"
+    # if 1: # test XYZ point cloud format
+    #     filename=PYTHON_FILE_PATH+"test_cloud_XYZ_noRGB.pcd"
+    # else: # test XYZRGB point cloud format
+    #     filename=PYTHON_FILE_PATH+"test_cloud_XYZRGB.pcd"
 
-    open3d_cloud = open3d.read_point_cloud(filename)
-    rospy.loginfo("Loading cloud from file by open3d.read_point_cloud: ")
-    print(open3d_cloud)
-    print("")
+    # print("Loading Open3D Cloud")
+    # open3d_cloud = open3d.io.read_point_cloud(filename)
 
-    # -- Set publisher
-    topic_name="kinect2/qhd/points"
-    pub = rospy.Publisher(topic_name, PointCloud2, queue_size=1)
+    # # -- Set publisher
+    # topic_name="kinect2/qhd/points"
+    # pub = rospy.Publisher(topic_name, PointCloud2, queue_size=1)
     
     # -- Set subscriber
     global received_ros_cloud
@@ -133,40 +115,32 @@ if __name__ == "__main__":
         global received_ros_cloud
         received_ros_cloud=ros_cloud
         rospy.loginfo("-- Received ROS PointCloud2 message.")
-    rospy.Subscriber(topic_name, PointCloud2, callback)      
+    rospy.Subscriber("/map_compress/full_cloud/3d", PointCloud2, callback)      
     
     # -- Convert open3d_cloud to ros_cloud, and publish. Until the subscribe receives it.
     while received_ros_cloud is None and not rospy.is_shutdown():
         rospy.loginfo("-- Not receiving ROS PointCloud2 message yet ...")
 
-        if 1: # Use the cloud from file
-            rospy.loginfo("Converting cloud from Open3d to ROS PointCloud2 ...")
-            ros_cloud = convertCloudFromOpen3dToRos(open3d_cloud)
+        # if 1: # Use the cloud from file
+        #     rospy.loginfo("Converting cloud from Open3d to ROS PointCloud2 ...")
+        #     ros_cloud = convertCloudFromOpen3dToRos(open3d_cloud)
 
-        else: # Use the cloud with 3 points generated below
-            rospy.loginfo("Converting a 3-point cloud into ROS PointCloud2 ...")
-            TEST_CLOUD_POINTS = [
-                [1.0, 0.0, 0.0, 0xff0000],
-                [0.0, 1.0, 0.0, 0x00ff00],
-                [0.0, 0.0, 1.0, 0x0000ff],
-            ]
-            ros_cloud = pc2.create_cloud(
-                Header(frame_id="odom"), FIELDS_XYZ , TEST_CLOUD_POINTS)
-
-        # publish cloud
-        pub.publish(ros_cloud)
-        rospy.loginfo("Conversion and publish success ...\n")
+        # # publish cloud
+        # pub.publish(ros_cloud)
         rospy.sleep(1)
         
-    # -- After subscribing the ros cloud, convert it back to open3d, and draw
+    print("Converting To Open3d")
     received_open3d_cloud = convertCloudFromRosToOpen3d(received_ros_cloud)
-    print(received_open3d_cloud)
 
-    # write to file
-    output_filename=PYTHON_FILE_PATH+"conversion_result.pcd"
-    open3d.write_point_cloud(output_filename, received_open3d_cloud)
-    rospy.loginfo("-- Write result point cloud to: "+output_filename)
-
-    # draw
-    open3d.draw_geometries([received_open3d_cloud])
-    rospy.loginfo("-- Finish display. The program is terminating ...\n")
+    # Poisson Surface Reconstruction
+    print("Estimating Normals")
+    received_open3d_cloud.estimate_normals()
+    received_open3d_cloud.orient_normals_consistent_tangent_plane(100)
+    # open3d.visualization.draw_geometries([received_open3d_cloud], point_show_normal=True)
+    mesh, densities = open3d.geometry.TriangleMesh.create_from_point_cloud_poisson(received_open3d_cloud)
+    # mesh = open3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(received_open3d_cloud, 0.03)
+    # mesh.compute_vertex_normals()
+    # # mesh.paint_uniform_color([0.706, 0.706, 0.706])
+    # open3d.visualization.draw_geometries([mesh])
+    # open3d.io.write_triangle_mesh(PYTHON_FILE_PATH+"example_mesh.ply", mesh)
+    open3d.io.write_point_cloud(PYTHON_FILE_PATH+"example_polygon.ply", received_open3d_cloud)
